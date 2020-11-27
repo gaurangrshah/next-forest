cf6711462b7c333260deaf3ddc4041430d1e14dd
gaurangrshah <gaurang.r.shah@gmail.com>
2020-11-27 00:09:11 -0500

    integrates chakra

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,32 @@
+import { useState } from 'react'
+import Head from 'next/head'
 import matter from 'gray-matter'
 import Layout from '../components/Layout'
 import BlogList from '../components/BlogList'
+import DefaultLayout from '../chakra/layouts/default'
 
 const Index = props => {
+  // const { setMsg, setError } = useToastDispatch()
+
+  // React.useEffect(() => {
+  //   setError({
+  //     title: 'Welcome.',
+  //     description: 'This is a test and can be triggered from anywhere',
+  //     status: 'success',
+  //     duration: 9000,
+  //     isClosable: true,
+  //   })
+  // }, [])
   return (
     <Layout
       pathname="/"
       siteTitle={props.title}
       siteDescription={props.description}
     >
+      <Head>
+        <title key="title">home</title>
+        <link rel="icon" href="/favicon.ico/" />
+      </Head>
       <section>
         <BlogList allBlogs={props.allBlogs} />
       </section>
